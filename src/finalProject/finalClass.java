5492fc46db50b33c8aad222789efb8bebed7256f
safinasuhrobi <safina0020@gmail.com>
2022-09-28 20:48:16 -0400

    2nd commit

--- src/finalProject/finalClass.java
+++ src/finalProject/finalClass.java
@@ -4,7 +4,7 @@ public class finalClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("it's my final class of git");
+		System.out.println("this is the project for mercedes");
 
 	}
 
